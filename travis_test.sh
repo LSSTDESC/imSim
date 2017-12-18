0836eeb6b6533453787e0d708e3fca702fc61c99
Jim Chiang <jchiang@slac.stanford.edu>
2017-12-18 15:06:06 -0800

    switch to docker builds

---
+++ travis_test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+source /lsst/stack/loadLSST.bash
+setup lsst_sims
+pip install nose
+pip install coveralls
+pip install pylint
+eups declare imsim -r /home/imSim -t current
+setup imsim
+cd /home/imSim
+scons
+nosetests -s --with-coverage --cover-package=desc.imsim
+pylint --py3k `find . -name \*.py -print`
