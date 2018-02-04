aa78a298958ab2013a48ba02cf268a58c6c8ea21
Jim Chiang <jchiang@slac.stanford.edu>
2018-02-04 15:43:57 -0800

    use lsstdesc/stack-sims docker image; set imSim directory so that coveralls can find coverage info

--- travis_test.sh
+++ travis_test.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-source /opt/lsst/software/stack/loadLSST.bash
+source scl_source enable devtoolset-6
+source loadLSST.bash
 setup lsst_sims
 pip install nose
 pip install coveralls
 pip install pylint
-eups declare imsim -r /home/imSim -t current
+eups declare imsim -r /home/travis/imSim -t current
 setup imsim
-cd /home/imSim
+cd /home/travis/imSim
 scons
 nosetests -s --with-coverage --cover-package=desc.imsim
