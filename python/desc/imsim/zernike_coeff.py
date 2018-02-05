72d938f4cb923c32546f13c0b1dd726c41948018
djperrefort <djperrefort@gmail.com>
2018-02-05 14:31:25 -0800

    Changes to class based interface

--- python/desc/imsim/zernike_coeff.py
+++ python/desc/imsim/zernike_coeff.py
@@ -1,42 +1,67 @@
 """
 Code to determine deviations in the Zernike coefficients determined by the
-AOS open loop control system. Results do not include contributions from the
-closed loop lookup table.
+AOS closed loop control system. Results do not include contributions from the
+open loop lookup table.
 """
 
 import os
 
 import numpy as np
 from scipy.interpolate import interp2d
-from scipy import optimize
+from scipy.optimize import leastsq
 
 FILE_DIR = os.path.dirname(os.path.realpath(__file__))
 MATRIX_PATH = os.path.join(FILE_DIR, 'sensitivity_matrix.txt')
 
 
-def get_sensitivity_matrix():
+def _gen_fit_func(p):
     """
-    Returns the sensitivity matrix as a (35, 19, 50) array
+    Uses given coefficients to generate a function representing a 2d polynomial
 
-    Dimension 0 represents 35 sampling positions in the focal plane.
+    @param [in] p is an array of 6 polynomial coefficients
 
-    Dimension 1 represents 19 Zernike polynomials from z=4 (Focus) to
-        z=22 (2nd Spherical).
-
-    Dimension 3 represents the 50 degrees of freedom in the optical system.
-
-    @param [out] a numpy array with shape (35, 19, 50)
+    @param [out] A function representing the polynomial
+        f(x, y) = p[0]x^2 + p[1]x + p[2]y^2 + p[3]y + p[4]xy + p[5]
     """
 
-    return np.genfromtxt(MATRIX_PATH).reshape((35, 19, 50))
+    def fit_func(x_arr, y_arr):
+        x_terms = p[0] * x_arr * x_arr + p[1] * x_arr
+        y_terms = p[2] * y_arr * y_arr + p[3] * y_arr
+        cross_terms = p[4] * x_arr * y_arr + p[5]
+        return x_terms + y_terms + cross_terms
+
+    return fit_func
 
 
-def mock_distortions(deviation):
+def _error_fit_func(p, x_arr, y_arr, z_arr):
     """
-    Returns an array of mock optical deviations as a (35, 50) array
+    Calculates the residuals of a 2d polynomial fit function
 
-    Deviations are simulated randomly at 35 positions in the LSST focal plane
-    for each of LSST's 50 optical degrees of freedom.
+    @param [in] p is an array of 6 polynomial coefficients for the fit
+
+    @param [in] x_arr is an array of x coordinates
+
+    @param [in] y_arr is an array of y coordinates
+
+    @param [in] z_val is an array of expected or measured values
+
+    @param [out] An array of the residuals
+    """
+
+    return _gen_fit_func(p)(x_arr, y_arr) - z_arr
+
+
+def moc_deviations(spread):
+    """
+    Returns an array of mock optical deviations as a (35, 50) array. Mock
+
+    For each optical degree of freedom in LSST, deviations are chosen randomly
+    at 35 positions in the focal plane using a uniform distribution. Maximum
+    values for each degree of freedom are chosen and hardcoded based on Angeli
+    et al. 2014. The minimum value is set using the deviation argument as
+    (1 - <deviation>) * <max distortion>.
+
+    @param [in] deviation defines the range in size of randomly generated
 
     @param [out] A numpy array representing mock optical distortions
     """
@@ -61,7 +86,7 @@ def mock_distortions(deviation):
         0.1, 0.1, 0.1, 0.1, 0.1,
     ])
 
-    min_distortion = (1 - 2 * deviation) * max_distortion
+    min_distortion = (1 - spread) * max_distortion
     random_signs = np.random.choice([-1, 1], size=50)
     distortion = np.zeros((35, 50))
     for i in range(35):
@@ -71,206 +96,115 @@ def mock_distortions(deviation):
     return distortion
 
 
-def cartesian_samp_coords():
-    """
-    Returns two lists with 35 cartesian sampling coordinates in the focal plane
+class ClosedLoopZ:
 
-    @param [out] a list of 35 x coordinates
+    sensitivity = np.genfromtxt(MATRIX_PATH).reshape((35, 19, 50))
 
-    @param [out] a list of 35 y coordinates
-    """
+    def __init__(self, deviations):
+        """
+        @param [in] deviations is a (35, 50) array representing deviations in
+        each optical degree of freedom at the 35 sampling coordinates
+        """
 
-    # Initialize with central point
-    x_list = [0.]
-    y_list = [0.]
+        self._sampling_coeff = self._calc_sampling_coeff(deviations)
+        self._fit_functions = self._gen_fit_functions()
 
-    # Loop over points on spines
-    radii = [0.379, 0.841, 1.237, 1.535, 1.708]
-    angles = [0, 60, 120, 180, 240, 300]
-    for radius in radii:
-        for angle in angles:
-            x_list.append(radius * np.cos(np.deg2rad(angle)))
-            y_list.append(radius * np.sin(np.deg2rad(angle)))
+    def _calc_sampling_coeff(self, deviations):
+        """
+        Calculates 19 zernike coefficients at 35 positions in the focal plane
 
-    # Add Corner raft points by hand
-    x_list.extend([1.185, -1.185, -1.185, 1.185])
-    y_list.extend([1.185, 1.185, -1.185, -1.185])
+        @param [in] deviations is a (35, 50) array representing deviations in
+        each optical degree of freedom at the 35 sampling coordinates
+        """
 
-    return np.array(x_list), np.array(y_list)
+        num_sampling_coords = self.sensitivity.shape[0]
+        num_zernike_coeff = self.sensitivity.shape[1]
 
+        coefficients = np.zeros((num_sampling_coords, num_zernike_coeff))
+        for i in range(num_sampling_coords):
+            coefficients[i] = self.sensitivity[i].dot(deviations[i])
 
-def polar_samp_coords():
-    """
-    Returns two lists with 35 polar sampling coordinates in the focal plane
+        return coefficients.transpose()
 
-    @param [out] a list of 35 r coordinates
+    @staticmethod
+    def cartesian_samp_coords():
+        """
+        Return 35 cartesian sampling coordinates in the focal plane
 
-    @param [out] a list of 35 theta coordinates
-    """
+        @param [out] an array of 35 x coordinates
 
-    # Initialize with central point
-    r_list = [0.]
-    theta_list = [0.]
+        @param [out] an array of 35 y coordinates
+        """
 
-    # Loop over points on spines
-    radii = [0.379, 0.841, 1.237, 1.535, 1.708]
-    angles = [0, 60, 120, 180, 240, 300]
-    for radius in radii:
-        for angle in angles:
-            r_list.append(radius)
-            theta_list.append(angle)
+        # Initialize with central point
+        x_list = [0.]
+        y_list = [0.]
 
-    # Add Corner raft points
-    x_raft_coords = [1.185, -1.185, -1.185, 1.185]
-    y_raft_coords = [1.185, 1.185, -1.185, -1.185]
-    for x, y in zip(x_raft_coords, y_raft_coords):
-        theta_list.append(np.arctan2(y, x))
-        r_list.append(np.sqrt(x * x + y * y))
+        # Loop over points on spines
+        radii = [0.379, 0.841, 1.237, 1.535, 1.708]
+        angles = [0, 60, 120, 180, 240, 300]
+        for radius in radii:
+            for angle in angles:
+                x_list.append(radius * np.cos(np.deg2rad(angle)))
+                y_list.append(radius * np.sin(np.deg2rad(angle)))
 
-    return np.array(r_list), np.array(theta_list)
+        # Add Corner raft points by hand
+        x_list.extend([1.185, -1.185, -1.185, 1.185])
+        y_list.extend([1.185, 1.185, -1.185, -1.185])
 
+        return np.array(x_list), np.array(y_list)
 
-def _raise_zernike_deviations(fp_x, fp_y, distortion_vectors):
-    """
-    Type and value check arguments for calculating zernike deviations
+    def _gen_fit_functions(self):
+        """
+        Generate a separate fit function for each zernike coefficient
 
-    @param [in] fp_x should be a float or int type
+        @param [out] A list of 19 functions
+        """
 
-    @param [in] fp_y should be a float or int type
+        out = []
+        x, y = self.cartesian_samp_coords()
+        for i, zernike_coeff in enumerate(self._sampling_coeff):
+            optimal = leastsq(_error_fit_func, np.arange(0, 6),
+                              args=(x, y, zernike_coeff))
 
-    @param [in] distortion_vectors should be an array with shape (35, 50)
-    """
+            fit_coeff = optimal[0]
+            out.append(_gen_fit_func(fit_coeff))
 
-    type_err = "Argument {} should be {} type."
-    if not isinstance(fp_x, (float, int)):
-        raise TypeError(type_err.format('fp_x', 'float'))
+        return out
 
-    if not isinstance(fp_y, (float, int)):
-        raise TypeError(type_err.format('fp_y', 'float'))
+    def interp_deviations(self, fp_x, fp_y, kind='linear'):
+        """
+        Determine the zernike coefficients at given coordinates by interpolating
 
-    # Todo: Change place holder value (100) to focal plane radius
-    r_focal_plane = 100
-    r_coordinates = fp_x * fp_x + fp_y + fp_y
-    if r_coordinates >= r_focal_plane:
-        raise ValueError('Provided coordinates are outside LSST focal plane')
+        @param [in] fp_x is the desired focal plane x coordinate
 
-    if not isinstance(distortion_vectors, np.ndarray):
-        raise TypeError(type_err.format('distortion_vectors', 'array'))
+        @param [in] fp_y is the desired focal plane y coordinate
 
-    distortions_shape = distortion_vectors.shape
-    if distortions_shape != (35, 50):
-        dist_val_err = 'Argument distortion_vector should have shape ' \
-                       '(35, 50). Found {} instead.'
+        @param [in] kind is the type of interpolation to perform. (eg. "cubic")
 
-        raise ValueError(dist_val_err.format(distortions_shape))
+        @param [out] An array of 19 zernike coefficients
+        """
 
+        x, y = self.cartesian_samp_coords()
+        num_zernike_coeff = self._sampling_coeff.shape[1]
 
-def interp_z_deviations(fp_x, fp_y, distortion_vectors):
-    """
-    Return the interpolated zernike coefficients at a focal plane position
+        out_arr = np.zeros((num_zernike_coeff,))
+        for i, coeff in enumerate(self._sampling_coeff):
+            interp_func = interp2d(x, y, coeff[i], kind=kind)
+            out_arr[i] = interp_func(fp_x, fp_y)[0]
 
-    @param [in] fp_x is the desired focal plane x coordinate
+        return out_arr
 
-    @param [in] fp_y is the desired focal plane y coordinate
+    def fit_coefficients(self, fp_x, fp_y):
+        """
+        Determine the zernike coefficients at given coordinates by fitting a 2d polynomial
 
-    @param [in] distortion_vectors is an array describing deviations in
-    each optical degree of freedom sampled at 35 positions.
-    It should have shape (35, 50).
+        @param [in] fp_x is the desired focal plane x coordinate
 
-    @param [out] A list of 19 zernike coefficients
-    """
+        @param [in] fp_y is the desired focal plane y coordinate
 
-    _raise_zernike_deviations(fp_x, fp_y, distortion_vectors)
+        @param [out] An array of 19 zernike coefficients
+        """
 
-    sensitivity_matrix = get_sensitivity_matrix()
-    num_sampling_positions = sensitivity_matrix.shape[0]
-    num_zernike_coefficients = sensitivity_matrix.shape[1]
+        return np.array([f(fp_x, fp_y) for f in self._fit_functions])
 
-    coefficients = np.zeros((num_sampling_positions, num_zernike_coefficients))
-    for i in range(num_sampling_positions):
-        coefficients[i] = sensitivity_matrix[i].dot(distortion_vectors[i])
-
-    out = []
-    x, y = cartesian_samp_coords()
-    for i in range(num_zernike_coefficients):
-        interp_func = interp2d(x, y, coefficients[:, i], kind='cubic')
-        out.extend(interp_func(fp_x, fp_y))
-
-    return out
-
-
-def _fit_func(p, x_arr, y_arr):
-    """
-    Calculates the value of a two dimensional, second order polynomial
-
-    @param [in] p is an array of 7 polynomial coefficients
-
-    @param [in] x_arr is an array of x coordinates
-
-    @param [in] y_arr is an array of y coordinates
-
-    @param [out] An array of the polynomial evaluated at x_arr and y_arr
-    """
-
-    x_terms = p[0] * x_arr * x_arr + p[1] * x_arr + p[2]
-    y_terms = p[3] * y_arr * y_arr + p[4] * y_arr + p[5]
-    cross_terms = p[6] * x_arr * y_arr
-    return x_terms + y_terms + cross_terms
-
-
-def _error_fit_func(p, x_arr, y_arr, z_arr):
-    """
-    Calculates the residual of a 2d fit using as:
-        _fit_func`(p, x_arr, y_arr) - z_val
-
-    @param [in] p is an array of the coefficients for the fit
-
-    @param [in] x_arr is an array of x coordinates
-
-    @param [in] y_arr is an array of y coordinates
-
-    @param [in] z_val is an array of the expected value of the fit evaluated
-        at x_arr and y_arr
-
-    @param [out] An array of the residuals
-    """
-
-    return _fit_func(p, x_arr, y_arr) - z_arr
-
-
-def fit_z_deviations(fp_x, fp_y, distortion_vectors):
-    """
-    Return the zernike coefficients at a focal plane position using a 2d fit
-
-    @param [in] fp_x is the desired focal plane x coordinate
-
-    @param [in] fp_y is the desired focal plane y coordinate
-
-    @param [in] distortion_vectors is an array describing deviations in
-    each optical degree of freedom sampled at 35 positions.
-    It should have shape (35, 50).
-
-    @param [out] A list of 19 zernike coefficients
-    """
-
-    _raise_zernike_deviations(fp_x, fp_y, distortion_vectors)
-
-    sensitivity_matrix = get_sensitivity_matrix()
-    num_sampling_positions = sensitivity_matrix.shape[0]
-    num_zernike_coefficients = sensitivity_matrix.shape[1]
-
-    coefficients = np.zeros((num_sampling_positions, num_zernike_coefficients))
-    for i in range(num_sampling_positions):
-        coefficients[i] = sensitivity_matrix[i].dot(distortion_vectors[i])
-
-    x, y = cartesian_samp_coords()
-    out = []
-    for i in range(num_zernike_coefficients):
-        optimal = optimize.leastsq(_error_fit_func, np.arange(0, 7),
-                                   args=(x, y, coefficients[:, i]))
-
-        fit_coeff = optimal[0]
-        fit_eval = _fit_func(fit_coeff, fp_x, fp_y)
-        out.append(fit_eval)
-
-    return out
