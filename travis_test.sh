6a01cfdd1a3132c92af1a5911820727dd0e28322
Jim Chiang <jchiang@slac.stanford.edu>
2018-02-04 16:24:53 -0800

    pass TRAVIS_BUILD_DIR env var to test script

--- travis_test.sh
+++ travis_test.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-package_dir=$( cd $(dirname $BASH_SOURCE)/..; pwd -P )
 source scl_source enable devtoolset-6
 source loadLSST.bash
 setup lsst_sims
 pip install nose
 pip install coveralls
 pip install pylint
-eups declare imsim -r ${package_dir} -t current
+eups declare imsim -r ${TRAVIS_BUILD_DIR} -t current
 setup imsim
-cd ${package_dir}
+cd ${TRAVIS_BUILD_DIR}
 scons
 nosetests -s --with-coverage --cover-package=desc.imsim
