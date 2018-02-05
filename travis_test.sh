82c4fff1b50aef2772cda09240a62ef9df6d3db3
Jim Chiang <jchiang@slac.stanford.edu>
2018-02-04 16:07:04 -0800

    use TRAVIS_BUILD_DIR env var to set package directory in container

--- travis_test.sh
+++ travis_test.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+package_dir=$( cd $(dirname $BASH_SOURCE)/..; pwd -P )
 source scl_source enable devtoolset-6
 source loadLSST.bash
 setup lsst_sims
 pip install nose
 pip install coveralls
 pip install pylint
-eups declare imsim -r /home/travis/imSim -t current
+eups declare imsim -r ${package_dir} -t current
 setup imsim
-cd /home/travis/imSim
+cd ${package_dir}
 scons
 nosetests -s --with-coverage --cover-package=desc.imsim
