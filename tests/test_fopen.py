f05e8955fc2d662b6e76a3c59c31c0b485aed57f
Jim Chiang <jchiang@slac.stanford.edu>
2018-03-23 11:36:53 -0700

    test for more than one includeobj line

--- tests/test_fopen.py
+++ tests/test_fopen.py
@@ -11,19 +11,26 @@ class FopenTestCase(unittest.TestCase):
         os.makedirs(self.test_dir, exist_ok=True)
         self.fopen_test_file \
             = os.path.join(self.test_dir, 'fopen_test_file.txt')
-        self.fopen_include_file \
-            = os.path.join(self.test_dir, 'fopen_include_file.txt.gz')
-        self.lines = 'line1 line2 line3'.split()
+        self.fopen_include_file1 \
+            = os.path.join(self.test_dir, 'fopen_include_file1.txt.gz')
+        self.fopen_include_file2 \
+            = os.path.join(self.test_dir, 'fopen_include_file2.txt.gz')
+        self.lines = 'line1 line2 line3 line4'.split()
         with open(self.fopen_test_file, 'w') as output:
             output.write('%s\n' % self.lines[0])
             output.write('%s\n' % self.lines[1])
             output.write('includeobj %s\n'
-                         % os.path.basename(self.fopen_include_file))
-        with gzip.open(self.fopen_include_file, 'wt') as output:
+                         % os.path.basename(self.fopen_include_file1))
+            output.write('includeobj %s\n'
+                         % os.path.basename(self.fopen_include_file2))
+        with gzip.open(self.fopen_include_file1, 'wt') as output:
             output.write('%s\n' % self.lines[2])
+        with gzip.open(self.fopen_include_file2, 'wt') as output:
+            output.write('%s\n' % self.lines[3])
 
     def tearDown(self):
-        for item in (self.fopen_test_file, self.fopen_include_file):
+        for item in (self.fopen_test_file, self.fopen_include_file1,
+                     self.fopen_include_file2):
             try:
                 os.remove(item)
             except OSError:
