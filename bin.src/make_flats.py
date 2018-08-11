15cf4868a43b727dcd19f6bf3720e0e6a942d2f4
Jim Chiang <jchiang@slac.stanford.edu>
2018-08-10 23:18:14 -0700

    add logging; allow for alternate wcs to be used

--- bin.src/make_flats.py
+++ bin.src/make_flats.py
@@ -9,7 +9,7 @@ import lsst.afw.cameraGeom as cameraGeom
 from lsst.sims.GalSimInterface import LSSTCameraWrapper, make_galsim_detector
 import desc.imsim
 
-parser = argparse.ArgumentParser("imSim flat production script.")
+parser = argparse.ArgumentParser(description="imSim flat production script.")
 parser.add_argument('instcat', type=str, help="instance catalog file")
 parser.add_argument('--sensors', type=str, default=None,
                     help="sensors to simulate")
@@ -24,15 +24,16 @@ parser.add_argument('--log_level', type=str,
 
 args = parser.parse_args()
 
+config = desc.imsim.read_config()
+
 obs_md, phot_params, _ = desc.imsim.parsePhoSimInstanceFile(args.instcat,
                                                             numRows=30)
 sensor_list = args.sensors.split('^') if args.sensors is not None \
     else args.sensors
 rng = galsim.UniformDeviate(args.seed)
-niter = int(args.counts_total/args.counter_per_iter + 0.5)
+niter = int(args.counts_total/args.counts_per_iter + 0.5)
 counts_per_iter = args.counts_total/niter
 logger = desc.imsim.get_logger(args.log_level, name='make_flats')
-config = desc.imsim.read_config()
 
 visit = obs_md.OpsimMetaData['obshistID']
 
@@ -40,12 +41,13 @@ camera_wrapper = LSSTCameraWrapper()
 
 for det in camera_wrapper.camera:
     det_name = det.getName()
-    logger.info("processing %s", det_name)
     if (det.getType() != cameraGeom.SCIENCE or
             (args.sensors is not None and det_name not in sensor_list)):
         continue
+    logger.info("processing %s", det_name)
     gs_det = make_galsim_detector(camera_wrapper, det_name, phot_params, obs_md)
     desc.imsim.add_treering_info([gs_det])
-    my_flat = desc.imsim.make_flat(gs_det, counts_per_iter, niter, rng)
+    my_flat = desc.imsim.make_flat(gs_det, counts_per_iter, niter, rng,
+                                   logger=logger, wcs=None)
     ccd_id = "R{}_S{}".format(det_name[2:5:2], det_name[8:11:2])
     my_flat.write('flat_{}_{}_{}.fits'.format(visit, ccd_id, obs_md.bandpass))
--- python/desc/imsim/flats.py
+++ python/desc/imsim/flats.py
@@ -2,10 +2,13 @@
 imSim module to make flats.
 """
 import galsim
+import desc.imsim
 
 __all__ = ['make_flat']
 
-def make_flat(gs_det, counts_per_iter, niter, rng, buf=2):
+
+def make_flat(gs_det, counts_per_iter, niter, rng, buf=2, logger=None,
+              wcs=None):
     """
     Create a flat by successively adding lower level flat sky images.
     This is based on
@@ -29,21 +32,29 @@ def make_flat(gs_det, counts_per_iter, niter, rng, buf=2):
     buf: int [2]
         Pixel buffer around each to account for charge redistribution
         across pixel boundaries.
+    logger: logging.Logger [None]
+        Logging object. If None, then a default logger will be used.
+    wcs: galsim.fitswcs.GSFitsWCS [None]
+        Alternative WCS object. If None, then gs_det.wcs will be used.
 
     Returns
     -------
     galsim.ImageF
     """
+    if logger is None:
+        logger = desc.imsim.get_logger('INFO', name='make_flat')
+    if wcs is None:
+        wcs = gs_det.wcs
     ncol = gs_det.xMaxPix - gs_det.xMinPix + 1
     nrow = gs_det.yMaxPix - gs_det.yMinPix + 1
-    flat = galsim.ImageF(ncol, nrow, wcs=gs_det.wcs)
+    flat = galsim.ImageF(ncol, nrow, wcs=wcs)
     sensor = galsim.SiliconSensor(rng=rng,
                                   treering_center=gs_det.tree_rings.center,
                                   treering_func=gs_det.tree_rings.func,
                                   transpose=True)
 
     # Create a noise-free base image to add at each iteration.
-    base_image = galsim.ImageF(ncol, nrow, wcs=gs_det.wcs)
+    base_image = galsim.ImageF(ncol, nrow, wcs=wcs)
     base_image.wcs.makeSkyImage(base_image, sky_level=1.)
     mean_pixel_area = base_image.array.mean()
     sky_level_per_iter = counts_per_iter/mean_pixel_area
@@ -60,15 +71,17 @@ def make_flat(gs_det, counts_per_iter, niter, rng, buf=2):
         xmin = i*dx + 1
         xmax = (i + 1)*dx
         for j in range(ny):
+            logger.info("amp: %d, %d", i, j)
             ymin = j*dy + 1
             ymax = (j + 1)*dy
-            temp_image = galsim.ImageF(ncol, nrow, wcs=gs_det.wcs)
+            temp_image = galsim.ImageF(ncol, nrow, wcs=wcs)
             bounds = (galsim.BoundsI(xmin-buf, xmax+buf, ymin-buf, ymax+buf)
                       & temp_image.bounds)
             temp_amp = temp_image[bounds]
-            for _ in range(niter):
+            for it in range(niter):
+                logger.debug("iter %d", it)
                 temp = sensor.calculate_pixel_areas(temp_amp)
-                temp /= temp.mean()
+                temp /= temp.array.mean()
                 temp *= base_image[bounds]
                 temp.addNoise(noise)
                 temp_amp += temp
