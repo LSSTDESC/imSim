4f49b6ef96c37d10917b3e232458b3e566a6db7b
Jim Chiang <jchiang@slac.stanford.edu>
2019-01-16 15:46:39 -0800

    build the version.py module explicitly since its build is part of the tests target

--- travis_test.sh
+++ travis_test.sh
@@ -8,7 +8,11 @@ pip install pylint
 git clone https://github.com/lsst/obs_lsst.git
 cd obs_lsst
 setup -r . -j
-scons lib python shebang examples doc policy
+# Build the obs_lsst package, but avoid the time consuming and lengthy
+# output from the 'tests' target.  The version module is built by the
+# 'tests', so build that explicitly since it is imported by the
+# package itself.
+scons lib python shebang examples doc policy python/lsst/obs/lsst/version.py
 cd ..
 eups declare imsim -r ${TRAVIS_BUILD_DIR} -t current
 setup imsim
