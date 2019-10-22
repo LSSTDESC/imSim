5d9b275c7f4a8cd65297e1fd280ee90f6c1868c1
James Chiang <jchiang87@gmail.com>
2019-10-22 15:45:28 -0700

    use DC2production eups tags for setting up throughputs and sims_skybrightness_data

--- travis_test.sh
+++ travis_test.sh
@@ -2,6 +2,8 @@
 source scl_source enable devtoolset-8
 source loadLSST.bash
 setup -t sims_w_2019_42 lsst_sims
+setup -t DC2production throughputs
+setup -t DC2production sims_skybrightness_data
 pip install nose
 pip install coveralls
 pip install pylint
