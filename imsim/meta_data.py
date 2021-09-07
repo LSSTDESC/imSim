3e75acdac957e3fac6a319bf62487a7fc842af52
Mike Jarvis <michael@jarvis.net>
2021-09-07 12:16:28 -0400

    Remove install_dir from meta_data.py

--- imsim/meta_data.py
+++ imsim/meta_data.py
@@ -2,7 +2,6 @@
 import os
 
 imsim_dir = os.path.split(os.path.realpath(__file__))[0]
-install_dir = os.path.split(imsim_dir)[0]
 
 if 'IMSIM_DATA_DIR' in os.environ: # pragma: no cover
     data_dir = os.environ['IMSIM_DATA_DIR']
