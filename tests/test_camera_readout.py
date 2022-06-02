c474db9e3b942c3dd27beef6f084f54489edaecd
Mike Jarvis <michael@jarvis.net>
2022-06-02 00:19:59 -0400

    Remove set_noao_keywords, which isn't used anywhere

--- tests/test_camera_readout.py
+++ tests/test_camera_readout.py
@@ -92,46 +92,5 @@ class ImageSourceTestCase(unittest.TestCase):
         os.remove(outfile)
 
 
-class NoaoKeywordTestCase(unittest.TestCase):
-    "TestCase class for raft-level NOAO mosaicking keywords"
-
-    def setUp(self):
-        pass
-
-    def tearDown(self):
-        pass
-
-    def test_set_noao_keywords(self):
-        "Test the set_noao_keywords function."
-        # Test that DETSIZE has been set so that appropriate vendor
-        # geometry can be used.
-        hdu = fits.ImageHDU()
-        hdu.name = 'Segment00'
-        self.assertRaises(RuntimeError, imsim.set_noao_keywords, hdu, 'S00')
-
-        # Test keyword values written by this code against real TS8
-        # raft level headers.
-        exclude = ('NAXIS BITPIX CHECKSUM HEADVER CHANNEL EXTNAME '
-                   'CCDSUM AVERAGE AVGBIAS STDVBIAS STDEV BSCALE BZERO '
-                   'DATASUM SLOT')
-        ccd_geoms = {'E2V': '[1:4096,1:4004]',
-                     'ITL': '[1:4072,1:4000]'}
-        for vendor, detsize in ccd_geoms.items():
-            for slot in ('S%i%i' % x for x in
-                         itertools.product(range(3), range(3))):
-                ref_file = str(DATA_DIR / ('%s_raft_example_%s.fits.gz'%(vendor, slot)))
-                with fits.open(ref_file) as ref:
-                    for amp in ('%i%i' % chan for chan in
-                                itertools.product((0, 1), range(8))):
-                        hdu = fits.ImageHDU()
-                        hdu.name = 'Segment%s' % amp
-                        hdu.header['DETSIZE'] = detsize
-                        hdu = imsim.set_noao_keywords(hdu, slot)
-                        for keyword in hdu.header.keys():
-                            if keyword not in exclude:
-                                self.assertEqual(hdu.header[keyword],
-                                                 ref[hdu.name].header[keyword])
-
-
 if __name__ == '__main__':
     unittest.main()
