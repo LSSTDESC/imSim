Fix batoid wcs coordsys including 180 deg rotation

imsim/photon_ops.py
@@ -133,7 +133,7 @@ def ray_vector_to_photon_array(
     Stores into an already existing galsim.PhotonArray out"""
 
     assert all(np.abs(ray_vector.z) < 1.0e-15)
-    out.x, out.y = focal_to_pixel(ray_vector.y * 1e3, -ray_vector.x * 1e3, detector)
+    out.x, out.y = focal_to_pixel(ray_vector.y * 1e3, ray_vector.x * 1e3, detector)
 
     out.dxdz = ray_vector.vx / ray_vector.vz
     out.dydz = ray_vector.vy / ray_vector.vz