Fix batoid wcs coordsys including 180 deg rotation

imsim/batoid_wcs.py
@@ -253,7 +253,7 @@ class BatoidWCSFactory:
         """WCS for converting between observed position and field angle.
         """
         cq, sq = np.cos(self.q), np.sin(self.q)
-        affine = galsim.AffineTransform(cq, -sq, sq, cq)
+        affine = galsim.AffineTransform(cq, sq, sq, -cq)
         return galsim.TanWCS(
             affine, self.obs_boresight, units=galsim.radians
         )
@@ -296,7 +296,7 @@ class BatoidWCSFactory:
         """
         return self.fiducial_telescope.withLocallyRotatedOptic(
             "LSSTCamera",
-            batoid.RotZ(-self.rotTelPos)
+            batoid.RotZ(self.rotTelPos)
         )
 
     def _field_to_focal(self, thx, thy):
@@ -318,9 +318,8 @@ class BatoidWCSFactory:
             wavelength=self.wavelength*1e-9
         )
         self._telescope.trace(rv)
-        # x -> -x to map batoid x to EDCS x.
         # x/y transpose to convert from EDCS to DVCS
-        return rv.y*1e3, -rv.x*1e3
+        return rv.y*1e3, rv.x*1e3
 
     def _focal_to_field(self, fpx, fpy):
         """

tests/test_batoid_wcs.py
@@ -217,6 +217,11 @@ def test_imsim():
         rtol=0, atol=30.0,
     )
 
+    # We accidentally simulated DC2 with the camera rotated 180 degrees too far.
+    # That includes the regression test data here.  So to fix the WCS code, but
+    # still use the same regression data, we need to add 180 degrees here.
+    rotTelPos += 180 * galsim.degrees
+
     # For actual WCS check, we use a factory that _does_ know about refraction.
     factory = imsim.BatoidWCSFactory(
         boresight, rotTelPos, obstime, fiducial_telescope, wavelength,
@@ -390,13 +395,13 @@ def test_intermediate_coord_sys():
         rtol=0, atol=1e-10
     )
     # Now decrease zenith angle, so moves higher in the sky.
-    # thx should stay the same and thy should increase.
+    # thx should stay the same and thy should decrease.
     dz = 0.001
     rc, dc = factory._observed_az_to_ICRF(aob, zob-dz)
     rob, dob = factory._ICRF_to_observed(rc, dc)
     thx, thy = factory._observed_to_field(rob, dob)
     np.testing.assert_allclose(thx, 0.0, rtol=0, atol=1e-14)
-    np.testing.assert_allclose(thy, dz, rtol=0, atol=1e-8)
+    np.testing.assert_allclose(thy, -dz, rtol=0, atol=1e-8)
 
     # Now increase azimuth angle, so moves towards the ground East.  What happens to thx, thy?
     dz = 0.001