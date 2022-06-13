caf8128fb3af0773f7789c4d3234c7c5b780e2b8
James Chiang <jchiang87@gmail.com>
2022-06-13 08:18:13 -0700

    implement Mike's suggested change to the test if an object is a point source

--- imsim/stamp.py
+++ imsim/stamp.py
@@ -68,8 +68,7 @@ class LSST_SiliconBuilder(StampBuilder):
             # For really faint things, don't try too hard.  Just use 32x32.
             image_size = 32
 
-        elif (not isinstance(gal, galsim.ChromaticSum)
-              and isinstance(gal._original, galsim.DeltaFunction)):
+        elif (hasattr(gal, 'original') and isinstance(gal.original, galsim.DeltaFunction)):
             # For bright stars, set the folding threshold for the
             # stamp size calculation.  Use a
             # Kolmogorov_and_Gaussian_PSF since it is faster to
