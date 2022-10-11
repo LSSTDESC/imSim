ef6cb8e075229c836384c730a83bf3e065f78ef2
Mike Jarvis <michael@jarvis.net>
2022-10-10 21:12:14 -0400

    Switch test_multiprocessing to use more canonical user interface for running config functionality

--- tests/test_multiprocessing.py
+++ tests/test_multiprocessing.py
@@ -3,11 +3,13 @@ Script to test that the sky catalog object lists match the
 CCD being simulated in multi-CCD runs.
 """
 import os
+import sys
 import glob
 import argparse
 from pathlib import Path
 import numpy as np
-from galsim.main import parse_args, make_logger, ReadConfig, process_config
+import logging
+import galsim
 
 def run_imsim(nfiles):
     """
@@ -26,32 +28,35 @@ def run_imsim(nfiles):
     opsim_db_file = os.path.join(imsim_dir, 'tests', 'data',
                                  'small_opsim_9683.db')
 
-    all_config = [{'template': template,
-                   'input.instance_catalog': '',
-                   'input.sky_catalog': {'file_name': skycatalog_file,
-                                         'obj_types': ['galaxy']},
-                   'input.opsim_meta_dict.file_name': opsim_db_file,
-                   'input.opsim_meta_dict.visit': 449053,
-                   'input.tree_rings.only_dets': ['R22_S11', 'R22_S12'],
-                   'image.random_seed': 42,
-                   'gal.type': 'SkyCatObj',
-                   'stamp.world_pos.type': 'SkyCatWorldPos',
-                   'stamp.fft_sb_thresh': '1e5',
-                   'output.camera': 'LsstCam',
-                   'output.det_num.first': 94,
-                   'output.nfiles': nfiles,
-                   'output.truth.dir': 'fits',
-                   'output.truth.file_name.format':
-                   'centroid_%08d-%1d-%s-%s-det%03d.txt'}]
+    logger = logging.getLogger('test_multiprocessing')
+    if len(logger.handlers) == 0:
+        logger.addHandler(logging.StreamHandler(sys.stdout))
+    logger.setLevel(logging.INFO)
+
+    config = {'template': template,
+              'input.instance_catalog': '',
+              'input.sky_catalog': {'file_name': skycatalog_file,
+                                    'obj_types': ['galaxy']},
+              'input.opsim_meta_dict.file_name': opsim_db_file,
+              'input.opsim_meta_dict.visit': 449053,
+              'input.tree_rings.only_dets': ['R22_S11', 'R22_S12'],
+              'image.random_seed': 42,
+              'gal.type': 'SkyCatObj',
+              'stamp.world_pos.type': 'SkyCatWorldPos',
+              'stamp.fft_sb_thresh': '1e5',
+              'output.camera': 'LsstCam',
+              'output.det_num.first': 94,
+              'output.nfiles': nfiles,
+              'output.truth.dir': 'fits',
+              'output.truth.file_name.format': 'centroid_%08d-%1d-%s-%s-det%03d.txt',
+            }
 
     args = argparse.Namespace(config_file='', variables=[],
                               verbosity=2, log_file=None, file_type=None,
                               module=None, profile=False, njobs=1, job=1,
                               except_abort=True, version=False)
 
-    logger = make_logger(args)
-
-    return process_config(all_config, args, logger)
+    galsim.config.Process(config, logger=logger)
 
 def cleanup():
     # Clean up the test output.
