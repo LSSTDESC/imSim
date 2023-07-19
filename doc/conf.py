c24436a74bfa8ec6316bfa62904ea9eca0321017
Chris Walter <chris.walter@duke.edu>
2023-07-19 16:54:35 -0400

    Make the tables reasonable.
    It fills me with rage I had to do this.

--- doc/conf.py
+++ doc/conf.py
@@ -9,6 +9,13 @@ import sphinx.ext.napoleon
 sys.path.insert(0, os.path.abspath(".."))
 
 
+html_static_path = ['_static']
+
+html_css_files = [
+    'custom.css',
+]
+
+
 def load_imsim_version():
     """Extract version of imsim without importing the whole imsim module"""
 
