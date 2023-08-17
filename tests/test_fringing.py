abf507dfb0bfee0307e99ad4316b97f3be833556
Mike Jarvis <michael@jarvis.net>
2023-08-17 18:18:14 -0400

    Add test that apply_fringing does the same thing as the manual run

--- tests/test_fringing.py
+++ tests/test_fringing.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pytest
-from imsim import make_batoid_wcs, CCD_Fringing
+from imsim import make_batoid_wcs, CCD_Fringing, get_camera
 import galsim
 
 def test_fringing():
@@ -15,10 +15,12 @@ def test_fringing():
     mjd = 60232.3635999295
     rottelpos = 350.946271812373
     band = 'y'
-    serial_num = 382
+    camera = get_camera()
+    det_name = 'R22_S11'
+    serial_num = camera[det_name].getSerial()
 
     xarr, yarr = np.meshgrid(range(4096), range(4004))
-    
+
     # Testing a CCD with an arbitrary location on the focal plane.
     ra = 54.86
     dec = -35.76
@@ -34,12 +36,11 @@ def test_fringing():
         },
     }
 
-    galsim.config.ProcessInput(config)
     image = galsim.config.BuildImage(config)
 
     ccd_fringing = CCD_Fringing(true_center=image.wcs.toWorld(image.true_center),
                                 boresight=world_center,
-                                seed=serial_num, spatial_vary=True)
+                                seed=hash(serial_num), spatial_vary=True)
     # Test zero value error
     with pytest.raises(ValueError):
         ccd_fringing.calculate_fringe_amplitude(xarr, yarr, amplitude=0)
@@ -53,13 +54,36 @@ def test_fringing():
     # Check the min/max of fringing varaition for the current offset.
     np.testing.assert_approx_equal(fringe_map.max(), 1.00205, significant=4)
     np.testing.assert_approx_equal(fringe_map.min(), 0.99794, significant=4)
-    
-    # Test when spatial vary is False. The fringe amplitude should be the same for 
+
+    # Actually make a fringing map with this:
+    sky_level = 1000
+    sky_image = galsim.Image(bounds=image.bounds, wcs=image.wcs, init_value=sky_level)
+    sky_image *= fringe_map
+
+    # Check that this is the same image that the config processing makes
+    config = {
+        'image': {
+            'type': 'LSST_Image',
+            'xsize': 4096,
+            'ysize': 4004,
+            'wcs': wcs,
+            'nobjects': 0,
+            'sky_level_pixel': sky_level,
+            'apply_fringing': True,
+            'boresight': world_center
+        },
+        'det_name': 'R22_S11'
+    }
+    config_sky_image = galsim.config.BuildImage(config)
+
+    np.testing.assert_allclose(config_sky_image.array, sky_image.array)
+
+    # Test when spatial vary is False. The fringe amplitude should be the same for
     # sensors at different locations
 
     ccd_fringing_1 = CCD_Fringing(true_center=image.wcs.toWorld(image.true_center),
                                 boresight=world_center,
-                                seed=serial_num, spatial_vary=False)
+                                seed=hash(serial_num), spatial_vary=False)
     fringe_map1 = ccd_fringing_1.calculate_fringe_amplitude(xarr,yarr)
 
     # Try another random location on the focal plane.
@@ -69,7 +93,7 @@ def test_fringing():
 
     ccd_fringing_2 = CCD_Fringing(true_center=image.wcs.toWorld(image.true_center),
                                 boresight=world_center,
-                                seed=serial_num, spatial_vary=False)
+                                seed=hash(serial_num), spatial_vary=False)
     fringe_map2 = ccd_fringing_2.calculate_fringe_amplitude(xarr,yarr)
     # Check if the two fringing maps are indentical.
     if np.array_equal(fringe_map1,fringe_map2) != True:
