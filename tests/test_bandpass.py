952920dcae9945215cacea2eb73bf92227be946b
Mike Jarvis <michael@jarvis.net>
2023-09-22 16:03:51 -0400

    Add a test of the fallback to galsim.Bandpass when RUBIN_SIM_DATA is not defined.

--- tests/test_bandpass.py
+++ tests/test_bandpass.py
@@ -1,6 +1,8 @@
 import numpy as np
 import galsim
 from imsim import RubinBandpass
+from unittest import mock
+from imsim_test_helpers import CaptureLog
 
 def test_rubin_bandpass():
     """Check the RubinBandpass functions to check that they are similar to GalSim lsst_*.dat files.
@@ -24,6 +26,22 @@ def test_rubin_bandpass():
             assert np.isclose(rubin_bp.blue_limit, galsim_bp.blue_limit, atol=15)
             assert np.isclose(rubin_bp.red_limit, galsim_bp.red_limit, atol=8)
             assert np.isclose(rubin_bp.effective_wavelength, galsim_bp.effective_wavelength, atol=3)
+
+
+        # If RUBIN_SIM_DATA isn't availble, then RubinBandpass reverts to the galsim files,
+        # and everything should match exactly.
+        with mock.patch('os.getenv', return_value=''):
+            with CaptureLog() as cl:
+                alt_rubin_bp = RubinBandpass(band, logger=cl.logger)
+        assert "Using the old bandpass files" in cl.output
+        print('Alt_Rubin_bp: ',alt_rubin_bp.blue_limit, alt_rubin_bp.effective_wavelength,
+              alt_rubin_bp.red_limit)
+        galsim_bp = galsim.Bandpass(f"LSST_{band}.dat", wave_type='nm')
+        galsim_bp = galsim_bp.truncate(relative_throughput=1.e-3)
+        galsim_bp = galsim_bp.thin()
+        galsim_bp = galsim_bp.withZeropoint('AB')
+        assert alt_rubin_bp == galsim_bp
+
 
 
 if __name__ == "__main__":
