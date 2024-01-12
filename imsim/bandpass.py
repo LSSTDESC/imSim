8b453e6b10ee0f2f62940b4b6e74243280d02ce7
Josh Meyers <jmeyers314@gmail.com>
2024-01-12 13:26:05 -0800

    Add airmass throughput interpolator

--- imsim/bandpass.py
+++ imsim/bandpass.py
@@ -1,12 +1,44 @@
-
+import numpy as np
 import os
+from pathlib import Path
 import galsim
 from galsim.config import RegisterBandpassType
 
 __all__ = ['RubinBandpass']
 
 
-def RubinBandpass(band, logger=None):
+class AtmInterpolator:
+    """ Interpolate atmospheric transmission curves from throughputs repo.
+    Linear interpolation of log(transmission) independently for every wavelength
+    does a good job.  Extrapolation is done by assuming a constant slope in
+    log(transmission).
+    """
+    def __init__(self, Xs, arr):
+        self.Xs = Xs
+        self.arr = arr
+        with np.errstate(all='ignore'):
+            self.logarr = np.log(arr)
+            self.log_extrapolation_slope = (
+                (self.logarr[-1] - self.logarr[-2])/(self.Xs[-1]-self.Xs[-2])
+            )
+
+    def __call__(self, X):
+        """ Evaluate atmospheric transmission curve at airmass X.
+        """
+        assert X >= 1.0
+        idx = np.searchsorted(self.Xs, X, side='right')
+        if idx == len(self.Xs):  # extrapolate
+            frac = (X - self.Xs[idx-1])
+            out = np.array(self.logarr[idx-1])
+            out += frac*self.log_extrapolation_slope
+        else:
+            frac = (X - self.Xs[idx-1]) / (self.Xs[idx] - self.Xs[idx-1])
+            out = (1-frac)*np.array(self.logarr[idx-1])
+            out += frac*self.logarr[idx]
+        return np.exp(out)
+
+
+def RubinBandpass(band, airmass=None, logger=None):
     """Return one of the Rubin bandpasses, specified by the single-letter name.
 
     The zeropoint is automatically set to the AB zeropoint normalization.
@@ -18,20 +50,38 @@ def RubinBandpass(band, logger=None):
     logger : logging.Logger
         If provided, a logger for logging debug statements.
     """
-    # This uses the baseline throughput files from lsst.sims
-    sims_dir = os.getenv("RUBIN_SIM_DATA_DIR")
-    file_name = os.path.join(sims_dir, "throughputs", "baseline", f"total_{band}.dat")
-    if not os.path.isfile(file_name):
-        # If the user doesn't have the RUBIN_SIM_DATA_DIR defined, or if they don't have
-        # the correct files installed, revert to the GalSim files.
-        logger = galsim.config.LoggerWrapper(logger)
-        logger.warning("Warning: Using the old bandpass files from GalSim, not lsst.sims")
-        file_name = f"LSST_{band}.dat"
-    bp = galsim.Bandpass(file_name, wave_type='nm')
+    path = Path(os.getenv("RUBIN_SIM_DATA_DIR")) / "throughputs"
+    if airmass is None:
+        file_name = path / "baseline" / f"total_{band}.dat"
+        if not file_name.is_file():
+            logger = galsim.config.LoggerWrapper(logger)
+            logger.warning("Warning: Using the old bandpass files from GalSim, not lsst.sims")
+            file_name = f"LSST_{band}.dat"
+        bp = galsim.Bandpass(str(file_name), wave_type='nm')
+    else:
+        # Could be more efficient by only reading in the bracketing airmasses,
+        # but probably doesn't matter much here.
+        atmos = {}
+        for f in (path / "atmos").glob("atmos_??_aerosol.dat"):
+            X = float(f.name[-14:-12])/10.0
+            wave, tput = np.genfromtxt(f).T
+            atmos[X] = wave, tput
+        Xs = np.array(sorted(atmos.keys()))
+        arr = np.array([atmos[X][1] for X in Xs])
+
+        interpolator = AtmInterpolator(Xs, arr)
+        tput = interpolator(airmass)
+        bp_atm = galsim.Bandpass(galsim.LookupTable(wave, tput), wave_type='nm')
+
+        file_name = path / "baseline" / f"hardware_{band}.dat"
+        wave, hardware_tput = np.genfromtxt(file_name).T
+        bp_hardware = galsim.Bandpass(galsim.LookupTable(wave, hardware_tput), wave_type='nm')
+        bp = bp_atm * bp_hardware
     bp = bp.truncate(relative_throughput=1.e-3)
     bp = bp.thin()
     bp = bp.withZeropoint('AB')
     return bp
+
 
 class RubinBandpassBuilder(galsim.config.BandpassBuilder):
     """A class for building a RubinBandpass in the config file
@@ -48,7 +98,10 @@ class RubinBandpassBuilder(galsim.config.BandpassBuilder):
             the constructed Bandpass object.
         """
         req = { 'band' : str }
-        kwargs, safe = galsim.config.GetAllParams(config, base, req=req)
+        opt = { 'airmass' : float }
+        kwargs, safe = galsim.config.GetAllParams(
+            config, base, req=req, opt=opt
+        )
         kwargs['logger'] = logger
         bp = RubinBandpass(**kwargs)
         logger.debug('bandpass = %s', bp)
