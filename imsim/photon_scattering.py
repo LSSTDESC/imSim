eaaad4e299aa2e43da61f8b0748c7d4c827c595a
William Lucas <w.lucas@epcc.ed.ac.uk>
2025-02-24 12:50:57 +0000

    Put scattered photons in focal plane coords.

--- imsim/photon_scattering.py
+++ imsim/photon_scattering.py
@@ -2,6 +2,9 @@ from galsim.config.extra import ExtraOutputBuilder, RegisterExtraOutput
 from galsim import PhotonArray
 from astropy.io.fits import BinTableHDU
 
+from .utils import pixel_to_focal
+from .camera import get_camera
+
 # An extra output type to enable two-pass photon scattering.
 # This output should be used in the first pass to write scattered photons to file,
 # while the second pass will read the scattered photons then go over all the images
@@ -19,22 +22,26 @@ class ScatteredPhotonsBuilder(ExtraOutputBuilder):
         # photon arrays to a single one and store in data, indexing by image number.
         if 'scattered_photons' in base and len(base['scattered_photons']) > 1:
             self.data[index] = PhotonArray.concatenate(base['scattered_photons'])
+            # We need to store the pixels' using focal plane coordinates so they
+            # can be accumulated in the second pass on an arbitrary sensor.
+            detector = get_camera(base['output']['camera'])[base['det_name']]
+            self.data[index].x, self.data[index].y = pixel_to_focal(self.data[index].x, self.data[index].y, detector)
         else:
             self.data[index] = PhotonArray(N=0)
 
     def finalize(self, config, base, main_data, logger):
         return self.data
-                
+
     def writeFile(self, file_name, config, base, logger):
         """Write the photon array to fits file.
-        Might it be faster to write to a pickle file?
+        Might it be faster/more space efficient to pickle?
         """
         for photon_array in self.final_data:
             photon_array.write(file_name)
 
     def writeHdu(self, config, base, logger):
-        """We don't want to write the scattered photons to FITS, so return an empty
-        BinTable in lieu of something else.
+        """We don't want to write the scattered photons to FITS, so return an
+        empty BinTable in lieu of something else.
         """
         return BinTableHDU(data=None)
 
